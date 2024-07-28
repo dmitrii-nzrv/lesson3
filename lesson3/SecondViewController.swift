60b1d365daa83bf4ca78de9460dce688fa9e1f59
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-07-28 05:14:43 +0300

    do UI for the first+second VCs

--- lesson3/SecondViewController.swift
+++ lesson3/SecondViewController.swift
@@ -21,7 +21,6 @@ class SecondViewController: UIViewController {
         return $0
     }(UILabel())
     
-    
     lazy var nameField: UITextField = {
         $0.frame = CGRect(x: viewWidth/15, y: nameLabel.frame.maxY, width: viewWidth-(viewWidth/15)*2, height: viewHeight/17)
         $0.placeholder = "haha"
@@ -48,14 +47,35 @@ class SecondViewController: UIViewController {
         
         return $0
     }(UITextField())
+    lazy var descriptionLabel: UILabel = {
+        $0.frame = CGRect(x: viewWidth/15 + 10, y: surnameField.frame.maxY+15, width: viewWidth/5, height: viewHeight/25)
+        $0.text = "Описание"
+        
+        return $0
+    }(UILabel())
     
+    lazy var descriptionTextField: UITextView = {
+        $0.frame = CGRect(x: viewWidth/15, y: descriptionLabel.frame.maxY, width: viewWidth-(viewWidth/15)*2, height: viewHeight/6)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 15
+        $0.backgroundColor = .placeholderText
+        return $0
+    }(UITextView())
+    
+    lazy var saveBtn: UIButton = {
+        $0.setTitle("Сохранить", for: .normal)
+        $0.frame = CGRect(x: viewWidth/15, y: viewHeight/1.15, width: viewWidth-(viewWidth/15)*2, height: viewHeight/16 )
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 20
+        $0.backgroundColor = .systemBlue
+        
+        return $0
+    }(UIButton())
     
     // MARK: Life cycle
     override func viewDidLoad() {
         title = "Настройка"
-        
         view.backgroundColor = .white
-        
         
         addSubviews()
     }
@@ -66,5 +86,8 @@ class SecondViewController: UIViewController {
         view.addSubview(nameField)
         view.addSubview(surnameLabel)
         view.addSubview(surnameField)
+        view.addSubview(descriptionLabel)
+        view.addSubview(descriptionTextField)
+        view.addSubview(saveBtn)
     }
 }
